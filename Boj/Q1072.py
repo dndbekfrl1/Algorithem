c96133e83a0a93c1602d710bbec43ec6b2ff5e98
dndbekfrl1 <dndbekfrl1@gmail.com>
2021-08-14 17:09:26 +0900

    Create Q1072.py

---
+++ Boj/Q1072.py
@@ -0,0 +1,23 @@
+x,y = map(int, input().split())
+z= int((y*100)/x) #현재 승률 
+
+l=1
+r=1000000000
+res=-1
+
+
+while(l<=r):
+    m = (l+r)//2
+    tmpX=x+m
+    tmpY=y+m
+    tmpZ=int((tmpY*100)/tmpX)
+
+    if tmpZ > z:
+        res=m
+        r=m-1
+    else:
+        l=m+1
+
+print(res)
+
+    
