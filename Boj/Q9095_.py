621c5df5ca7ecf15c2e4d9461c651805462ae392
dndbekfrl1 <dndbekfrl1@gmail.com>
2021-08-19 12:00:01 +0900

    Create Q9095_.py

---
+++ Boj/Q9095_.py
@@ -0,0 +1,17 @@
+t=int(input())
+
+dp=[0 for _ in range(12)]
+dp[0]=0
+dp[1]=1
+dp[2]=2
+dp[3]=4
+
+for i in range(4,12):
+    dp[i]=dp[i-3]+dp[i-2]+dp[i-1]
+
+for _ in range(t):
+    n=int(input())
+    print(dp[n])
+
+     
+            
